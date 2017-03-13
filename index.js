52d5dec3500e6d741e238a5e58e504d4eca3cf1c
Valerie Balcom <vqbalcom@gmail.com>
2017-03-13 16:36:58 -0700

    Add util inspection

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var sugarcoat = require( 'sugarcoat' );
 var config = require( './documentation/config.js' );
 
 sugarcoat( config ).then( function( data ) {
-    console.log('finished!', data);
+    
+    console.log( require( 'util' ).inspect( data, { depth: 5, colors: true } ) );
 });
