f6d1540627b29f71163edcbdad8ffae69e1f6d26
Ali Groening <agroening@sapient.com>
2017-03-31 14:50:57 -0700

    added a catch since sc no longer handles errors

--- index.js
+++ index.js
@@ -5,4 +5,14 @@ var config = require( './documentation/config.js' );
 sugarcoat( config ).then( function( data ) {
     
     console.log( require( 'util' ).inspect( data, { depth: 8, colors: true } ) );
+
+}).catch( errors => {
+
+    if ( errors.length ) {
+        
+        errors.forEach( ( error, index ) => {
+
+            console.log(index, error);
+        });
+    }
 });
